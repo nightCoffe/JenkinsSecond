a9e99e714e7a999e131c98bee31e3b9b1e68fecd
Your Name <youremail@domain.com>
2021-12-11 09:31:34 -0800

    added OwnerTests

---
+++ src/test/java/tests/properties/OwnerTests.java
@@ -0,0 +1,19 @@
+package tests.properties;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+    public CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void readCredentialTest() {
+        String login = credentials.login();
+        String password = credentials.password();
+
+        System.out.println(login);
+        System.out.println(password);
+    }
+}
