4a004fb6fd8432fa239b195f01f34617839b09a9
Your Name <youremail@domain.com>
2021-12-11 09:36:03 -0800

    added @Tag("properties") in OwnerTests

--- src/test/java/tests/properties/OwnerTests.java
+++ src/test/java/tests/properties/OwnerTests.java
@@ -2,8 +2,10 @@ package tests.properties;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("properties")
 public class OwnerTests {
     public CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
